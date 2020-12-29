1220b36c5f1e8d0271c9364b25a3e40513b06035
theerapol130199 <meen.130199@rmutsvmail.com>
2020-12-29 11:42:15 +0700

    Lab2

---
+++ src/LAB2/Exercise_STD_Profile.java
@@ -0,0 +1,37 @@
+package LAB2;
+
+import java.util.Scanner;
+
+public class Exercise_STD_Profile {
+    public static void main(String[] args) {
+
+
+        Scanner name = new Scanner(System.in);
+        System.out.print("Name: ");
+        String a = name.nextLine();
+
+        Scanner stdid = new Scanner(System.in);
+        System.out.print("ID: ");
+        String b = name.nextLine();
+
+        Scanner Age = new Scanner(System.in);
+        System.out.print("Age: ");
+        int c = Integer.parseInt(name.nextLine());
+
+        Scanner tel = new Scanner(System.in);
+        System.out.print("tel: ");
+        String d = name.nextLine();
+
+        Scanner email = new Scanner(System.in);
+        System.out.print("Email: ");
+        String e = name.nextLine();
+
+
+
+
+
+
+    }
+
+
+}
